0c2f8c5c1d65ea4b616e02e93367f0e5fde4d2a0
KirillMaslyakov <143893465+KirillMaslyakov@users.noreply.github.com>
2023-12-13 15:46:07 +0300

    Add files via upload

---
+++ Food_test/js/script.js
@@ -0,0 +1,114 @@
+const tabs = document.querySelectorAll('.tabheader__item');
+const tabContent = document.querySelectorAll('.tabcontent');
+const tabParent = document.querySelector('.tabheader');
+
+function hideTabContent(){
+    tabContent.forEach(item =>{
+        item.style.display = "none";
+    })
+    tabs.forEach(item=>{
+        item.classList.remove('tabheader__item_active');
+    })
+}
+
+function showTabContent(i=0){
+    tabContent[i].style.display = 'block';
+    tabs[i].classList.add('tabheader__item_active');
+}
+
+
+hideTabContent();
+showTabContent();
+
+tabParent.addEventListener('click',(event)=>{
+    if(event.target = event.target.classList.contains('tabheader__item')){
+        tabs.forEach((item,i)=>{
+            if(event.target == item){
+                hideTabContent();
+                showTabContent(i);
+            }
+        })
+    }
+})
+
+
+//modal
+
+const modalBth = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalClose= document.querySelector('[data-close]');
+modalBth.forEach(item=>{    
+    item.addEventListener('click', ()=>{
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    })
+    })
+
+    modalClose.addEventListener('click', ()=>{
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    })
+
+modal.addEventListener('click', (event)=>{
+    if(event.target === modal){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+})
+
+document.addEventListener('keydown', (event)=>{
+    if(event.code === 'Escape'){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+})
+
+
+//slider
+const slides = document.querySelectorAll('.offer-slider'),
+prev = document.querySelector('.offer-slider-prev'),
+next = document.querySelector('.offer-slider-next'),
+total = document.querySelector('#total')
+current = document.querySelector('#current');
+
+let slideIndex = 1;
+
+function showSlides(n){
+
+    if(n>slides.length){
+        slideIndex = 1;
+    }
+    if(n<slides.length){
+        slideIndex = SLIDES.length;
+    }
+
+    if(slides.length < 10){
+        total.textContent = `0${slides.length}`;
+    }else{
+        total.textContent = slides.length;
+    }
+    slides.forEach(item=> item.style.display="none");
+    slides[slideIndex-1].style.display="block"
+
+    if(slides.length <10){
+        current.textContent = `0${slideIndex}`;
+    }else{
+        current.textContent = slideIndex;
+    }
+}
+showSlides(slideIndex);
+
+function plusSlide(n){
+    showSlides(slideIndex=slideIndex+n);
+}
+
+next.addEventListener('click', ()=>{
+    plusSlide(1)
+})
+prev.addEventListener('click', ()=>{
+    plusSlide(-1)
+})
